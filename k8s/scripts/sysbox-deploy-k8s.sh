ac0dbd9bee28d75bf41c71244272254995ac878f
fykyx521 <446897682@qq.com>
2026-07-29 12:11:40 +0800

    fix: support Ubuntu 26.04 installer artifacts

--- k8s/scripts/sysbox-deploy-k8s.sh
+++ k8s/scripts/sysbox-deploy-k8s.sh
@@ -287,7 +287,8 @@ function get_artifacts_dir() {
 
 	local distro=$os_distro_release
 
-	if [[ "$distro" == "ubuntu-24.04" ]] ||
+	if [[ "$distro" == "ubuntu-26.04" ]] ||
+		[[ "$distro" == "ubuntu-24.04" ]] ||
 		[[ "$distro" == "ubuntu-22.04" ]] ||
 		[[ "$distro" == "ubuntu-21.10" ]] ||
 		[[ "$distro" == "ubuntu-20.04" ]] ||
